ca4350c2eb9a0aae5ef115d9910c44fcf671cafd
Nagornaya <o.nagornaya7@gmail.com>
2023-12-13 17:09:28 +0300

    Правки урла для видео и правки тайтла в регистрации

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import type { FC } from 'react';
 import s from './Hero.module.scss';
+import {useLocation} from "react-router-dom";
 
 const Hero: FC = () => {
+   const videoUrl = window.location.href.includes('github.io')
+       ? "/christmas-test/video/christmas.mp4"
+       : "/video/christmas.mp4";
+
   return (
       <div>
           <video autoPlay
@@ -10,7 +15,7 @@ const Hero: FC = () => {
                  playsInline
                  muted
                  preload="auto"
-                 src="/video/christmas.mp4"
+                 src={videoUrl}
                  className={s.video}
           />
       </div>
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import type { FC } from 'react';
 import LoginForm from "../../components/Forms/LoginForm/LoginForm";
-
+import s from './LoginPage.module.scss';
 const LoginPage: FC = () => {
   return (
       <div>
-        <h1>Страница авторизации</h1>
+        <h1 className={s.title}>Страница авторизации</h1>
         <LoginForm />
       </div>
   );
